b3fa0228e8ba13c81e02d9657b8699e85a10b13b
Harsh Sangrulkar <harshsangrulkar@gmail.com>
2023-10-15 23:52:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Body from "./components/Body";
 
 function App() {
+  const [date, setDate] = useState("");
+  const [age, setAge] = useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Body date={date} setDate={setDate} age={age} setAge={setAge} />
     </div>
   );
 }
